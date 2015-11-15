b95ab50926f7aa0603e9d7db6a994e25cc8179bc
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2015-11-14 23:18:49 -0800

    add totalDonuts sum array method

--- scripts.js
+++ scripts.js
@@ -8,9 +8,10 @@ function TopPotStore(loc, minCustomer, maxCustomer, avgDonuts) {
 	this.maxCustomer = maxCustomer;
 	this.avgDonuts = avgDonuts;
 	this.dPurchasedEachHour = [];
+
 }
 
-// Method generates a random number of customers
+// Method generates a random number of customers. Can this be combined with donutsPerHour??
 TopPotStore.prototype.customerGenerator = function(){
 	return Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
 }
@@ -24,14 +25,28 @@ TopPotStore.prototype.donutsPerHour = function(store){
 	};
 }
 
+// Method generates total donuts needed for one store.
+TopPotStore.prototype.totalDonuts = function(store) {
+	var total = store.dPurchasedEachHour.reduce(function(a,b){
+		return a + b;
+	});
+	return total;
+}
+
 // Store locations
 var downtown = new TopPotStore("Downtown", 8, 43, 4.50);
 downtown.donutsPerHour(downtown);
-console.log(downtown.dPurchasedEachHour);
+downtown.totalDonuts(downtown);
 
 var capitolHill = new TopPotStore("Capitol Hill", 4, 37, 2.00);
-var southLakeUnion = new TopPotStore("South Lake Union", 9, 23, 6.33);
-var wedgewood = new TopPotStore("Wedgewood", 2, 28, 1.25);
-var ballard = new TopPotStore("Ballard", 8, 58, 3.75);
+capitolHill.donutsPerHour(capitolHill);
 
+var southLakeUnion = new TopPotStore("South Lake Union", 9, 23, 6.33);
+southLakeUnion.donutsPerHour(southLakeUnion);
+
+var wedgewood = new TopPotStore("Wedgewood", 2, 28, 1.25);
+wedgewood.donutsPerHour(wedgewood);
+
+var ballard = new TopPotStore("Ballard", 8, 58, 3.75);
+ballard.donutsPerHour(ballard);
 
