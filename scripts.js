5686148ab7e9ae308272b4e892e9f203e43db221
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2015-11-14 22:17:47 -0800

    add donutsPerHour method, test array via print

--- scripts.js
+++ scripts.js
@@ -7,24 +7,31 @@ function TopPotStore(loc, minCustomer, maxCustomer, avgDonuts) {
 	this.minCustomer = minCustomer;
 	this.maxCustomer = maxCustomer;
 	this.avgDonuts = avgDonuts;
-	
+	this.dPurchasedEachHour = [];
 }
 
+// Method generates a random number of customers
 TopPotStore.prototype.customerGenerator = function(){
-	console.log(Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1)) + this.minCustomer);
+	return Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
 }
 
+// Method generates array of donuts sold per hour
+TopPotStore.prototype.donutsPerHour = function(store){
+	for(var i = 0; i < 12; i++) {
+		var customersThisHour = store.customerGenerator();
+		var donutsPurchased = Math.round(customersThisHour * this.avgDonuts);
+		this.dPurchasedEachHour[i] = donutsPurchased;
+	};
+}
+
+// Store locations
 var downtown = new TopPotStore("Downtown", 8, 43, 4.50);
-downtown.customerGenerator();
+downtown.donutsPerHour(downtown);
+console.log(downtown.dPurchasedEachHour);
 
 var capitolHill = new TopPotStore("Capitol Hill", 4, 37, 2.00);
-capitolHill.customerGenerator();
-
 var southLakeUnion = new TopPotStore("South Lake Union", 9, 23, 6.33);
-southLakeUnion.customerGenerator();
-
-var wedgeweood = new TopPotStore("Wedgewood", 2, 28, 1.25);
-wedgewood.customerGenerator();
-
+var wedgewood = new TopPotStore("Wedgewood", 2, 28, 1.25);
 var ballard = new TopPotStore("Ballard", 8, 58, 3.75);
-ballard.customerGenerator();
+
+
