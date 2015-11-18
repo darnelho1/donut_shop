efad686d62d31bfcc6fc37e92f22dec758bbc394
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2015-11-18 11:58:37 -0800

    add proto to constructor

--- scripts.js
+++ scripts.js
@@ -10,34 +10,19 @@ function TopPotStore(loc, minCustomer, maxCustomer, avgDonuts) {
 	this.dPurchasedEachHour = [];
 	this.sumDonuts = 0;
 
-}
-
-// Method generates a random number of customers. Can this be combined with donutsPerHour??
-/*TopPotStore.prototype.customerGenerator = function(){
-	return Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
-} */
-
-// Method generates array of donuts sold per hour
-TopPotStore.prototype.donutsPerHour = function(){
+	this.donutsPerHour = function(){
 	for(var i = 0; i < 12; i++) {
 		var customersThisHour = Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
-		//var customersThisHour = store.customerGenerator();
 		var donutsPurchased = Math.round(customersThisHour * this.avgDonuts);
 		this.dPurchasedEachHour[i] = donutsPurchased;
 		this.sumDonuts += donutsPurchased;
 	};
 }
 
-// Method generates total donuts needed for one store.
-/*TopPotStore.prototype.totalDonuts = function(store) {
-	var total = store.dPurchasedEachHour.reduce(function(a,b){
-		return a + b;
-	});
-	this.sumDonuts = total;
-}*/
+}
 
 // Render function
-TopPotStore.prototype.render = function(store) {
+TopPotStore.prototype.render = function() {
 	var tr = document.createElement('tr');
 	var td = document.createElement('td');
 	td.innerHTML = this.loc;
@@ -59,28 +44,24 @@ TopPotStore.prototype.render = function(store) {
 // Store locations
 var downtown = new TopPotStore("Downtown", 8, 43, 4.50);
 downtown.donutsPerHour();
-//downtown.totalDonuts(downtown);
 downtown.render();
 
 var capitolHill = new TopPotStore("Capitol Hill", 4, 37, 2.00);
 capitolHill.donutsPerHour();
-//capitolHill.totalDonuts(capitolHill);
 capitolHill.render();
 
 var southLakeUnion = new TopPotStore("South Lake Union", 9, 23, 6.33);
 southLakeUnion.donutsPerHour();
-//southLakeUnion.totalDonuts(southLakeUnion);
 southLakeUnion.render();
 
 var wedgewood = new TopPotStore("Wedgewood", 2, 28, 1.25);
 wedgewood.donutsPerHour();
-//wedgewood.totalDonuts(wedgewood);
 wedgewood.render();
 
 var ballard = new TopPotStore("Ballard", 8, 58, 3.75);
 ballard.donutsPerHour();
-//ballard.totalDonuts(ballard);
 ballard.render();
+
 
 // Top Pot Store Array
 var storeArray = [downtown, capitolHill, southLakeUnion, wedgewood, ballard];
@@ -94,15 +75,8 @@ var updateStores = function() {
 	var avgDonutsAdd = document.getElementById('avgDonutsAdd').value;
 	var newStoreObject = new TopPotStore(locInfo,parseInt(minCustAdd),parseInt(maxCustAdd),parseInt(avgDonutsAdd));
 	newStoreObject.donutsPerHour();
-	newStoreObject.totalDonuts(newStoreObject);
 	newStoreObject.render();
 	storeArray.push(newStoreObject);
-
-	downtown.minCustomer = 7;
-	downtown.maxCustomer = 35;
-	downtown.avgDonuts = 3;
-	downtown.donutsPerHour();
-	downtown.totalDonuts(downtown);
 }
 
 
